59b04781d78c9fd3516d8f74d93a0bfca749bcdf
arswarum <bajda.dmytro@gmail.com>
2022-05-16 14:38:45 +0200

    booking availability function

--- B_M_S/settings.py
+++ B_M_S/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'booking.apps.BookingConfig',
+    'booking.booking_functions',
 
     'phonenumber_field',
 ]
---
+++ booking/booking_functions/availability.py
@@ -0,0 +1,14 @@
+import datetime
+from booking.models import Room, Booking
+
+def check_availability(room, checkin, checkout):
+    avail_list = []
+    booking_list = Booking.objects.filter(room=room)
+
+    for booking in booking_list:
+        if booking.checkin > checkout or booking.checkout < checkin:
+            avail_list.append(True)
+        else:
+            avail_list.append(False)
+    
+    return all(avail_list)
